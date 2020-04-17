725f639d50702591c3613b755b81053863e32ca9
Lenard Frommelt <lenny09918050@googlemail.com>
2020-04-17 11:47:56 +0200

    added files

---
+++ ressources/copyscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## This script is called to copy all ressources
+## Destination is basicly the output/ module
+## The output module is the document-root of what is served on deployment
+
+cp sources/ressources/favicons/* output/
+
+echo 0
---
+++ sourceInfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+    getDependencies: () => {
+        return {
+            "crypto-hash": "^1.2.2",
+            "decamelize": "^4.0.0"
+        }
+
+    }
+}
